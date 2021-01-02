cfa61ead36c86bf72eb8ad755aa433236f5dcf0c
Lucas <laksteelhouse@gmail.com>
2021-01-02 18:05:00 +0100

    Adding UI fields to HTML, and .type and .click() tests

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -1,6 +1,9 @@
 describe('User can check their Bmi', () => {
 	it('user can enter weight and height', () => {
 		cy.visit('http://localhost:4000')
-		cy.contains('BMI Calculator')
+        cy.contains('BMI Calculator')
+        cy.get('#weight').type('90')
+        cy.get('#height').type('186')
+        cy.get('#Calculate').click()
 	})
 })
